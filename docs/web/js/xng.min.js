70c582594c39d2df396f679cb65e0643c6504475
Matthias Schmid <ramsondon@gmail.com>
2017-05-29 21:17:30 +0200

    added favicon

--- docs/web/js/xng.min.js
+++ docs/web/js/xng.min.js
@@ -1,5 +1,5 @@
 /*!
- * ramsondon.github.io - xng.js v1.0.0 (https://ramsondon.github.io/xng)
+ * ramsondon.github.io - xng.js v1.0.1 (https://ramsondon.github.io/xng)
  * Copyright 2017 -2017 Matthias Schimd <ramsondon@gmail.com> (https://ramsondon.github.io)
  * Licensed under MIT (https://github.com/ramsondon/xng/blob/master/LICENSE)
  */
