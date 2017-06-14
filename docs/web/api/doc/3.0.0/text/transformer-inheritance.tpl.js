54d45633b78d8f7e3c4bed46c3e939b4808633f5
Matthias Schmid <ramsondon@gmail.com>
2017-06-14 09:15:18 +0200

    refactored code to fit into width

--- docs/web/api/doc/3.0.0/text/transformer-inheritance.tpl.js
+++ docs/web/api/doc/3.0.0/text/transformer-inheritance.tpl.js
@@ -1,15 +1,18 @@
 
-// create a new Transformer
+// create a new transformer
 var myJsonTransformer = function() {};
 
-// inherit of the default Transformer
-myJsonTransformer.prototype = Object.create(_.xng.defaultTransformer().prototype);
+// get the protoype of the xng default transformer
+var dtp = _.xng.defaultTransformer().prototype;
+
+// inherit your new transformer from the default transformer
+myJsonTransformer.prototype = Object.create(dtp);
 
 // implement the doTransformation function
 myJsonTransformer.prototype.doTransformation = function (str) {
 	return JSON.parse(str);
 };
 
-// instead of a function you can assign your own Transformer class.
+// instead of a function you can assign your own transformer class instance.
 _.xng.transform("json_string", new myJsonTransformer());
 
